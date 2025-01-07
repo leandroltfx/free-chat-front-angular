799d62e0f2a2ab3b59a0a2e5608937a0e858370a
leandroltfx <leandroltfbr@hotmail.com>
2025-01-07 10:18:07 -0300

    criando formulário de login

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -10,6 +17,13 @@ import { LoginRoutingModule } from './login-routing.module';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
 
     LoginRoutingModule,
   ]
