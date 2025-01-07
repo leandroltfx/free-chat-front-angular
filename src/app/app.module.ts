611c05ff4f29228f16f7edc0d633d11b3608cd1e
leandroltfx <leandroltfbr@hotmail.com>
2025-01-07 09:38:53 -0300

    configurando lib de componentes Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
 
     AppRoutingModule,
   ],
